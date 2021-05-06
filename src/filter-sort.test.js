330a70630039d9778975922402845dfd49721c2e
Matthew Saber <mattsaber21@yahoo.com>
2021-05-06 07:01:21 -0700

    finished filter-sort

--- src/filter-sort.test.js
+++ src/filter-sort.test.js
@@ -1,4 +1,4 @@
-import { fiveAndGreaterOnly, fiveCharactersOrFewerOnly } from './array-filter';
+import { fiveAndGreaterOnly, fiveCharactersOrFewerOnly, evensOnly } from './filter-sort.js';
 
 test('Five and greater only', () => {
   const input = [3, 6, 8, 2]; // arrange
@@ -10,4 +10,10 @@ test('new array with 5 or fewer characters', () => {
   const input = ['dog', 'wolf', 'by', 'family', 'eaten', 'camping']; // arrange
   const output = fiveCharactersOrFewerOnly(input); // act
   expect(output).toEqual(['by', 'dog', 'wolf', 'eaten']); // assert
+});
+
+test('evensOnly', () => {
+  const input = [3, 6, 8, 2]; // arrange
+  const output = evensOnly(input); // act
+  expect(output).toEqual([6, 8, 2]); // assert
 });
